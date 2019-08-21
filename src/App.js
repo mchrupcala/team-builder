3bb5e384ce78ba9a3a81a19df5d14a7936c5ff94
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-08-21 15:42:01 -0400

    Initialized the project for today. Added a Form.js and data.js file for the team member list.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [member, setMember] = useState([]);
+
+  return <div className="App">=</div>;
+}
+
+export default App;
